c19c9bd4a946ab0e688aad3dedc1af0d05df8800
Ubuntu <ubuntu@ip-172-31-26-112.us-east-2.compute.internal>
2021-10-27 03:41:44 +0000

    update the first commit

--- DRL/actor.py
+++ DRL/actor.py
@@ -82,13 +82,14 @@ class ResNet(nn.Module):
         self.in_planes = 64
 
         block, num_blocks = cfg(depth)
+
         self.conv1 = conv3x3(num_inputs, 64, 2)
         self.bn1 = nn.BatchNorm2d(64)
         self.layer1 = self._make_layer(block, 64, num_blocks[0], stride=2)
         self.layer2 = self._make_layer(block, 128, num_blocks[1], stride=2)
         self.layer3 = self._make_layer(block, 256, num_blocks[2], stride=2)
         self.layer4 = self._make_layer(block, 512, num_blocks[3], stride=2)
-        self.fc = nn.Linear(512, num_outputs)
+        self.fc = nn.Linear(512 * block.expansion, num_outputs)
 
     def _make_layer(self, block, planes, num_blocks, stride):
         strides = [stride] + [1]*(num_blocks-1)
