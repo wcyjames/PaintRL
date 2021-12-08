2490a9b2152b473725434d22e969ffc371017307
momo7773 <yiqingt7773@gmail.com>
2021-12-08 14:32:05 -0800

    debug

--- DRL/evaluator.py
+++ DRL/evaluator.py
@@ -3,26 +3,26 @@ from utils.util import *
 
 class Evaluator(object):
 
-    def __init__(self, args, writer):    
+    def __init__(self, args, writer):
         self.validate_episodes = args.validate_episodes
         self.max_step = args.max_step
         self.env_batch = args.env_batch
         self.writer = writer
         self.log = 0
 
-    def __call__(self, env, policy, debug=False):        
+    def __call__(self, env, policy, debug=False):
         observation = None
         for episode in range(self.validate_episodes):
             # reset at the start of episode
             observation, mask = env.reset(test=True, episode=episode)
             episode_steps = 0
-            episode_reward = 0.     
-            assert observation is not None            
+            episode_reward = 0.
+            assert observation is not None
             # start episode
             episode_reward = np.zeros(self.env_batch)
             while (episode_steps < self.max_step or not self.max_step):
                 action = policy(observation)
-                observation, reward, done, (step_num), mask = env.step(action, episode_steps)
+                observation, reward, done, (step_num), mask = env.step(action)
                 episode_reward += reward
                 episode_steps += 1
                 env.save_image(self.log, episode_steps)
